36d4ff8f474606eade1218c81c934cb14b6e3095
Alexandra Marks <amarks93@gmail.com>
2021-10-07 15:55:45 -0400

    chore: add more buttons to navigate

--- client/App.js
+++ client/App.js
@@ -9,6 +9,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { home, login, signup, body, moles, singleMole, entry } from './NavigationConstants';
 import Login from './components/Login';
 import Entry from './components/Entry';
+import Signup from './components/Signup';
+import Body from './components/Body';
+import Moles from './components/Moles';
+import SingleMole from './components/SingleMole';
 
 // this is a dummy component for now
 import Home from './components/Home';
@@ -22,10 +26,10 @@ export default function App() {
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name={home} component={Home} />
           <Stack.Screen name={login} component={Login} />
-          {/* <Stack.Screen name={signup} component={Signup} />
+          <Stack.Screen name={signup} component={Signup} />
           <Stack.Screen name={body} component={Body} />
           <Stack.Screen name={moles} component={Moles} />
-          <Stack.Scren name={singleMole} component={SingleMole} /> */}
+          <Stack.Screen name={singleMole} component={SingleMole} />
           <Stack.Screen name={entry} component={Entry} />
         </Stack.Navigator>
       </NavigationContainer>
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, StatusBar, TouchableOpacity } from 'react-native';
-import { login, entry } from '../NavigationConstants';
+import { login, signup, body, moles, singleMole, entry } from '../NavigationConstants';
 
 const Home = (props) => {
   const navigation = props.navigation;
@@ -8,6 +8,18 @@ const Home = (props) => {
     <View style={styles.container}>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(login)}>
         Login
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(signup)}>
+        Signup
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(body)}>
+        Body
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(moles)}>
+        Moles
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(singleMole)}>
+        SingleMole
       </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(entry)}>
         Entry
