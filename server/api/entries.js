e5cc76cb17d174c1bb36173cefdb9828eebe0525
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-10-07 14:20:46 -0700

    feat: entries api routes, get all, get one, delete, put, post

---
+++ server/api/entries.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const {
+  models: { Entry },
+} = require("../db");
+module.exports = router;
+
+// GET one /api/entries/:entryId
+router.get("/:entryId", async (req, res, next) => {
+  try {
+    const entry = await Entry.findByPk(req.params.entryId);
+    res.json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// GET all /api/entries/all/:moleId
+router.get("/all/:moleId", async (req, res, next) => {
+  try {
+    const entries = await Entry.findAll({
+      where: {
+        moleId: req.params.moleId,
+      },
+    });
+    res.json(entries);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT /api/entries/:entryId
+router.put("/:entryId", async (req, res, next) => {
+  try {
+    const entry = await Entry.findByPk(req.params.entryId);
+    res.json(await entry.update(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST /api/entries
+router.post("/", async (req, res, next) => {
+  try {
+    res.status(201).json(await Entry.create(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE /api/entries/:entryId
+router.delete("/:entryId", async (req, res, next) => {
+  try {
+    const entry = await Entry.findByPk(req.params.entryId);
+    res.json(await entry.destroy());
+  } catch (err) {
+    next(err);
+  }
+});
