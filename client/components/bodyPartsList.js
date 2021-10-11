7176aacdbddd3a64014d056458774b2c80579fc0
Gigi Markley <gigimarkley@Admins-MacBook-Pro.local>
2021-10-11 07:43:22 -0700

    Feat: Moles now only renders the body parts that currently has a mole.

--- client/components/bodyPartsList.js
+++ client/components/bodyPartsList.js
@@ -1,63 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { fetchAllMoles } from '../store/mole';
-import { useDispatch, useSelector } from 'react-redux';
-import Moles from './moles';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { fetchAllMoles } from "../store/mole";
+import { useDispatch, useSelector } from "react-redux";
+import Moles from "./moles";
 
 const BodyPartsList = () => {
-	let moles = useSelector((state) => state.allMoles);
-	console.log('getting moles', moles);
+  let moles = useSelector((state) => state.allMoles);
+  let bodyParts = [
+    "head",
+    "torso",
+    "arm-l",
+    "arm-r",
+    "leg-l",
+    "leg-r",
+    "groin",
+    "butt",
+  ];
+  const dispatch = useDispatch();
 
-	let bodyParts = [
-		'head',
-		'torso',
-		'arm-l',
-		'arm-r',
-		'leg-l',
-		'leg-r',
-		'groin',
-		'butt',
-	];
+  //Currently hard coded for user 1. Add individual user functionality later.
+  useEffect(() => {
+    dispatch(fetchAllMoles(1));
+  }, []);
 
-	const dispatch = useDispatch();
+  const list = () => {
+    return bodyParts.map((bodyPart, index) => {
+      let molesInBodyPart = moles.filter((mole) => mole.bodyPart === bodyPart);
 
-	//Currently hard coded for user 1. Add individual user functionality later.
-	useEffect(() => {
-		dispatch(fetchAllMoles(1));
-	}, []);
-
-	const list = () => {
-		return bodyParts.map((bodyPart, index) => {
-			console.log(bodyPart);
-			let moleys = moles.filter((mole) => mole.bodyPart === bodyPart);
-			console.log('moleys', moleys);
-
-			return (
-				<View key={index}>
-					<Text style={styles.title}>{bodyPart}</Text>
-					<Text>
-						<Moles moles={moleys} />
-					</Text>
-				</View>
-			);
-		});
-	};
-	return <View>{list()}</View>;
+      if (molesInBodyPart.length)
+        return (
+          <View key={index}>
+            <Text style={styles.title}>{bodyPart}</Text>
+            <Text>
+              <Moles moles={molesInBodyPart} />
+            </Text>
+          </View>
+        );
+    });
+  };
+  return <View>{list()}</View>;
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	title: {
-		flex: 1,
-		fontSize: 25,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    flex: 1,
+    fontSize: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
 export default BodyPartsList;
