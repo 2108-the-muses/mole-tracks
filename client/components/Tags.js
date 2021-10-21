380301f74255a25d91a8bea9605550aa0f451ad3
Duchess <veraruthkahn@gmail.com>
2021-10-21 16:04:42 -0400

    heroku set up

--- client/components/Tags.js
+++ client/components/Tags.js
@@ -37,7 +37,7 @@ const Tags = ({
   const diameterTagSelect = (value) => {
     value === diameterTag ? setDiameterTag("") : setDiameterTag(value);
   };
-
+ 
   return (
     <View style={styles.tagsInAddEntryContainer}>
       <View style={styles.tagsCategoryContainer}>
