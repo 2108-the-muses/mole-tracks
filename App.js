96bbcea9f8d150a2bf55af547f54c30cfed6b6a6
Duchess <veraruthkahn@gmail.com>
2021-10-20 20:58:31 -0400

    images preload (?) but fonts are gone....

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import store from "./client/store";
 import Main from "./client/screens/Main";
 import { LogBox } from "react-native";
+
+import { Asset } from "expo-asset";
 LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
@@ -10,22 +12,34 @@ import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
 export default function App() {
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  const fetchFonts = () => {
-    return Font.loadAsync({
-      "SulphurPoint-Bold": require("./assets/fonts/SulphurPoint-Bold.ttf"),
-      "SulphurPoint-Light": require("./assets/fonts/SulphurPoint-Light.ttf"),
-      "SulphurPoint-Regular": require("./assets/fonts/SulphurPoint-Regular.ttf"),
-      "SF-Pro": require("./assets/fonts/SF-Pro.ttf"),
-    });
+  const [isLoaded, setIsLoaded] = useState(false);
+useEffect(()=>{console.log("loaded")},[isLoaded])
+  const fonts = {
+    "SulphurPoint-Bold": require("./assets/fonts/SulphurPoint-Bold.ttf"),
+    "SulphurPoint-Light": require("./assets/fonts/SulphurPoint-Light.ttf"),
+    "SulphurPoint-Regular": require("./assets/fonts/SulphurPoint-Regular.ttf"),
+    "SF-Pro": require("./assets/fonts/SF-Pro.ttf"),
   };
+  const images = [require("./assets/images/face-with-mole.png"),require("./assets/images/body-front.png"),require("./assets/images/body-back.png"),"https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/59232/mole-in-hole-clipart-xl.png"];
 
-  if (!dataLoaded) {
+const loadAssets = async()=>{
+  console.log("in load asset")
+    const fontAssets = fonts.map(font=>Font.loadAsync(font))
+    const imageAssets = images.map(image => {
+      if (typeof image === 'string') {
+        return Image.prefetch(image);
+      } else {
+        return Asset.fromModule(image).downloadAsync();
+      }
+    });
+    await Promise.all([...imageAssets, fontAssets])
+}
+
+  if (!isLoaded) {
     return (
       <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
+        startAsync={loadAssets}
+        onFinish={() => setIsLoaded(true)}
         onError={(err) => console.log(err)}
       />
     );
---
+++ XApp.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { Provider } from "react-redux";
+import store from "./client/store";
+import Main from "./client/screens/Main";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  const fetchFonts = () => {
+    return Font.loadAsync({
+      "SulphurPoint-Bold": require("./assets/fonts/SulphurPoint-Bold.ttf"),
+      "SulphurPoint-Light": require("./assets/fonts/SulphurPoint-Light.ttf"),
+      "SulphurPoint-Regular": require("./assets/fonts/SulphurPoint-Regular.ttf"),
+      "SF-Pro": require("./assets/fonts/SF-Pro.ttf"),
+    });
+  };
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+}
